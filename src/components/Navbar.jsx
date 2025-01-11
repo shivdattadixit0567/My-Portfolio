02210d22df081249575c374ae33e0978b37eba67
shivdatta dixit <shivdattadixit0567@gmail.com>
2025-01-11 12:46:44 +0530

    deploy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,47 +2,55 @@ import React from "react";
 import { Link } from "react-router";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 function Navbar() {
+  const [open, setOpen] = React.useState(false);
+  const show = () => {
+    setOpen(!open);
+  };
   return (
     <div className="sticky top-0 z-10">
-      <div className="flex justify-around bg-white h-[3rem] items-center">
+      <div className="flex md:justify-around bg-white md:h-[4rem] md:items-center justify-between p-2 h-[3rem]">
         <h1>logo</h1>
-        <ul className="flex">
-          <li className="mr-14">
-            <AnchorLink href="#home">
-              <button className="hover:bg-indigo-500 hover:text-white hover:rounded-3xl p-2 active:drop-shadow-2xl">
-                Home
-              </button>
-            </AnchorLink>
-          </li>
-          <li className="mr-14">
-            <AnchorLink href="#about">
-              <button className="hover:bg-indigo-500 hover:text-white hover:rounded-3xl p-2 active:drop-shadow-2xl">
-                About
-              </button>
-            </AnchorLink>
-          </li>
-          <li className="mr-14">
-            <AnchorLink href="#skill">
-              <button className="hover:bg-indigo-500 hover:text-white hover:rounded-3xl p-2 active:drop-shadow-2xl">
-                Skill
-              </button>
-            </AnchorLink>
-          </li>
-          <li className="mr-14">
-            <AnchorLink href="#project">
-              <button className="hover:bg-indigo-500 hover:text-white hover:rounded-3xl p-2 active:drop-shadow-2xl">
-                Project
-              </button>
-            </AnchorLink>
-          </li>
-          <li className="mr-14">
-            <AnchorLink href="#contact">
-              <button className="hover:bg-indigo-500 hover:text-white hover:rounded-3xl p-2 active:drop-shadow-2xl">
-                Contact
-              </button>
-            </AnchorLink>
-          </li>
-        </ul>
+        <div className="w-28 md:w-auto">
+          <button onClick={show} className="md:hidden pl-3">
+            <img className="w-5 h-5" src="/menu-burger.png" alt="" />
+          </button>
+          {!open ? (
+            <div className="bg-white flex flex-col md:flex-row md:gap-8">
+              <AnchorLink
+                className="w-full hover:bg-indigo-500 hover:text-white md:p-1 active:drop-shadow-2xl"
+                href="#home"
+              >
+                <button className="p-2">Home</button>
+              </AnchorLink>
+              <AnchorLink
+                className="w-full hover:bg-indigo-500 hover:text-white md:p-1 active:drop-shadow-2xl"
+                href="#about"
+              >
+                <button className="p-2">About</button>
+              </AnchorLink>
+              <AnchorLink
+                className="w-full hover:bg-indigo-500 hover:text-white md:p-1 active:drop-shadow-2xl"
+                href="#skill"
+              >
+                <button className="p-2">Skill</button>
+              </AnchorLink>
+              <AnchorLink
+                className="w-full hover:bg-indigo-500 hover:text-white md:p-1 active:drop-shadow-2xl"
+                href="#project"
+              >
+                <button className="p-2">Project</button>
+              </AnchorLink>
+              <AnchorLink
+                className="w-full hover:bg-indigo-500 hover:text-white md:p-1 active:drop-shadow-2xl"
+                href="#contact"
+              >
+                <button className="p-2">Contact</button>
+              </AnchorLink>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
     </div>
   );
